9a5b82e00157ff841de3923a503cfd375f0782e9
shintarokabe <kabe0852@gmail.com>
2021-02-21 10:02:39 +0000

    update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,6 +26,16 @@ def destroy
   redirect_to books_path
 end
 
+def edit
+  @book = Book.find(params[:id])
+end
+
+def update
+  @book = Book.find(params[:id])
+  @book.update(book_params)
+  redirect_to books_path
+end
+
 private
 
   def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # }
   resources :users, only: [:index, :show, :edit, :update]
 
-  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
 
 
 end
